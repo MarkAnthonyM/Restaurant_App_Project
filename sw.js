819867b9f6afa0d0d9385b7877c072569423ae02
Mark Anthony <markanthonymartinez9@gmail.com>
2018-10-17 09:17:15 -0500

    Add service worker fetching logic

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const staticCacheName = 'restaurant-static-v1';
 
+//Builds cache of assest from visted pages
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
@@ -23,8 +24,34 @@ self.addEventListener('install', function(event) {
         '/img/7.jpg',
         '/img/8.jpg',
         '/img/9.jpg',
-        '/img/10.jpg'
+        '/img/10.jpg',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
       ]);
+    }).catch(function(error) {
+      console.log(error);
+    })
+  );
+});
+
+//fetches cached assests for offline display
+self.addEventListener('fetch', function(event) {
+  let requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname.startsWith('/restaurant.html')) {
+      event.respondWith(caches.match('/restaurant.html'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
     }).catch(function(error) {
       console.log(error);
     })
